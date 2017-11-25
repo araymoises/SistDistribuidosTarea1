a25630ca5613a587a2bfde25f403394e87d8d864
Jorge Mujica <riontdev@gmail.com>
2017-11-25 08:56:39 -0400

    add TCP python server

--- TCP/Python/TCP.py
+++ TCP/Python/TCP.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 import socket
 
-TCP_IP = '192.168.0.108'
-TCP_PORT = 5005
+TCP_IP = '192.168.0.106'
+TCP_PORT = 9877
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-
+s.listen(5)
+print 'Esperando conexiones...'
 conn, addr = s.accept()
-print 'Connection address:', addr
+print 'Conexiones address:', addr
 while 1:
-	data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data:", data
-    conn.send(data)  # echo
+	data = conn.recv()
+	if data: break
+	print "recibi data:", data
+	conn.send(data)  # echo
 conn.close()
